ee76b618ed269be56ba9bc9810ec63a924326c60
Ben Selby <bselby@uwaterloo.ca>
2013-08-22 19:12:03 -0400

    adding basic functionality to undistort code

--- Makefile
+++ Makefile
@@ -1,2 +1,3 @@
 default:
 	g++ -o unwrap unwrap.cpp `pkg-config opencv --libs --cflags`
+	g++ -o undistort undistort.cpp `pkg-config opencv --libs --cflags`
--- undistort.cpp
+++ undistort.cpp
@@ -11,15 +11,16 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
+#include <fstream>
 #include <stdio.h>
 #include <sys/time.h>
 #include <string>
 
 #define PI 3.141592654
 
-// define the parameters for the output image
-const int WIDTH = 465;
-const int HEIGHT = WIDTH;
+// define the parameters for the output images
+const int WIDTH = 1425;
+const int HEIGHT = 116;
 
 // specify input and output locations:
 const std::string output_path = "output/";
@@ -36,29 +37,47 @@ int get_time_diff( struct timeval *result, struct timeval *t1, struct timeval *t
 
 int main( int argc, char** argv )
 {
-	int num_stripes = 0;
-	
-	if ( argc < 2 ) 
+
+	if ( argc < 4 ) 
     {
-        printf( "Usage: %s <image_filename> <number of stripes>\n", argv[0] );
+        printf( "Usage: %s <image_filename> <cal_input_data.txt> <number_of_lines>\n", argv[0] );
         return -1;
+    }    
+    
+    // Read the pixel values of the lines from the text file and store them in
+    // the array y_vals:
+    int num_lines = atoi( argv[3] );
+    printf("Number of lines: %d\n", num_lines);
+    std::ifstream input_data( argv[2] );
+    std::string line;
+    int y_vals[num_lines*2];
+    
+    if ( input_data.is_open() )
+    {
+    	int index = 0;
+    	while ( input_data.good() )
+    	{
+    		getline( input_data, line );
+    		y_vals[index] = atoi( line.c_str() );
+    		printf("Line %d: %d\n", index, y_vals[index] );
+    		index++;    		
+    	}
+    }
+    else
+    {
+    	printf( "Unable to open the file \"%s\" - exiting.\n", argv[2] ); 
+    	return -1;
     }
     
-    if ( argc < 3 )
-    {
-    	num_stripes = 7;
-    }
-	else
-	{
-		num_stripes = argv[2];
-	}
-	
-	cv::Mat src, undistorted_img;
+    input_data.close();
+    
+	printf("Successfully read the calibration file.\n");
+	cv::Mat src, top_img, bottom_img;
+	cv::Mat top_raw, bottom_raw; // for storing the top and bottom of the src
 	cv::Mat map_x, map_y;
-	
 	struct timeval start_time, end_time, time_diff, calc_time;
 	gettimeofday(&start_time, NULL);
-	
+
 	// Load the specified source image
 	std::string path = argv[1];
 	src = cv::imread( path, 1 );
@@ -68,59 +87,56 @@ int main( int argc, char** argv )
 		return -1;
 	}
 
-	undistorted_img.create( HEIGHT, WIDTH, src.type() );
-	map_x.create( undistorted_img.size(), CV_32FC1 );
-	map_y.create( undistorted_img.size(), CV_32FC1 );
-	
-	// develop the map arrays for the unwarping from polar coordinates
-	// i = y coord, j = x coord	
-	int rows = unwrapped_img.rows;
-	int cols = unwrapped_img.cols;
-	
-    for ( int i = 0; i < rows; i++ )
-    {
-        for ( int j = 0; j < cols; j++ )
-        {
-        	double theta = (double) j / RADIUS;// - (3*PI/2); // discretization in radians
-        	map_x.at<float>(rows-i,j) = RADIUS + i*sin(theta);
-        	map_y.at<float>(rows-i,j) = RADIUS + i*cos(theta);
-        }
-    }
-	
+	top_img.create( HEIGHT, WIDTH, src.type() );
+	bottom_img.create( HEIGHT, WIDTH, src.type() );
+
 	gettimeofday( &calc_time, NULL);
-	// let OpenCV handle the interpolation for the gaps in the unwrapped image
-    cv::remap( cropped_img, unwrapped_img, 
-    		 map_x,
-    		 map_y,
-    		 CV_INTER_LINEAR,
-    		 cv::BORDER_CONSTANT,
-    		 cv::Scalar(0,0,0)
-		   );
-   
+
+	cv::Rect ROI_top( 0, 16, WIDTH, 75 );
+	top_raw = src( ROI_top );
+	cv::Rect ROI_bottom( 0, 110, WIDTH, 40 );
+	bottom_raw = src( ROI_bottom );
+
+	cv::resize(top_raw, top_img, top_img.size() );		
+	cv::resize(bottom_raw, bottom_img, bottom_img.size() );
+	  
 	gettimeofday( &end_time, NULL );
 	get_time_diff( &time_diff, &start_time, &end_time );
     printf( "Time Elapsed: %ld.%06ld seconds\n", time_diff.tv_sec, time_diff.tv_usec );
     
     get_time_diff( &time_diff, &calc_time, &end_time );
     printf( "Remap time: %ld.%06ld seconds\n", time_diff.tv_sec, time_diff.tv_usec );
-    
+
+
+	// Display the images 
     cvNamedWindow( "Original", 1 );
     imshow( "Original", src );
-    cvNamedWindow( "undistorted", 1 );
-    imshow( "undistorted", undistorted_img );
+    
+    cvNamedWindow( "Top Mirror", 1 );
+    imshow( "Top Mirror", top_raw );
+    
+    cvNamedWindow( "Top Stereo", 1 );
+    imshow( "Top Stereo", top_img );
+    
+    cvNamedWindow( "Bottom Stereo", 1 );
+    imshow( "Bottom Stereo", bottom_img );
+    
+    cvNamedWindow( "Bottom Mirror", 1 );    
+    imshow( "Bottom Mirror", bottom_raw );
+
 	
-	int saved = cv::waitKey(0);
+	cv::waitKey(0);
 	
 	// if 's' key is pressed, save the unwrapped image
-	if ( saved == 1179731 || saved == 1048691 )
-	{	
-	    int last_index = path.find_last_of("/");
-    	std::string img_name = path.substr(last_index+1, path.length()-1);
-		std::string prefix = "undistorted_";
-		img_name = prefix + img_name; 
-		std::string out_path = output_path + img_name;
-		std::cout<<"Saved unwrapped image to: "<<out_path<<std::endl;
-		imwrite(out_path, undistorted_img);		
-	}
+//	if ( saved == 1179731 || saved == 1048691 )
+//	{	
+//	    int last_index = path.find_last_of("/");
+//    	std::string img_name = path.substr(last_index+1, path.length()-1);
+//		std::string prefix = "undistorted_";
+//		img_name = prefix + img_name; 
+//		std::string out_path = output_path + img_name;
+//		std::cout<<"Saved undistorted image to: "<<out_path<<std::endl;
+//		imwrite(out_path, undistorted_img);		
+//	}
 	return 0;
 }
